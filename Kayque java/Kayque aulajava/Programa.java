63a8504e233319e638bc9f18651c66d1ae8f3f25
kayque <clara.moreira.rodrigues@escola.pr.gov.br>
2026-05-22 11:09:30 -0300

    Add files via upload

---
+++ Kayque java/Kayque aulajava/Programa.java
@@ -0,0 +1,12 @@
+public class Programa {
+    public static void main(String[] args) {
+        String nome = "Kayque";
+        int idade = 15;
+        double altura = 1.60;
+        
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        
+    }
+}
---
+++ Kayque java/Kayque aulajava/programa.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Executado o programa java....
+java -cp "C:\Users\Aluno\Desktop\Kayque aulajava" Programa
+pause
