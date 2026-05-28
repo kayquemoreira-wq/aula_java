1083a3761aa48281a36da505a8ea19d5b1b2a5bf
kayque <clara.moreira.rodrigues@escola.pr.gov.br>
2026-05-28 11:45:28 -0300

    Add files via upload

---
+++ cadastro/src/main/java/com/example/cadastro/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.cadastro;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String cadastro(
+        @RequestParam String name,
+        @RequestParam String curso
+    ) {
+        return ""
+        + "<h1>Cadastro Realizado!</h1>"
+        + "<p>Nome: " + name + "</p>"
+        + "<p>Curso: " + curso + "<p/>";
+    }
+}
